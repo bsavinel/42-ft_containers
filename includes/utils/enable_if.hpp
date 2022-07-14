b613cec37997a8eb0c8265d377ab76959d40a550
Benoit Savinel <bsavinel@student.42.fr>
2022-07-14 16:19:06 +0200

    implemente: enable_if

--- includes/utils/enable_if.hpp
+++ includes/utils/enable_if.hpp
@@ -6,16 +6,27 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 11:05:08 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/07/14 16:10:36 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/07/14 16:11:59 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENABLE_IF_HPP
 # define ENABLE_IF_HPP
 
+//? https://eli.thegreenplace.net/2014/sfinae-and-enable_if
+
 namespace ft
 {
-
+	template<bool Cond, class T = void>
+	struct enable_if 
+	{
+	};
+	
+	template<class T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
 }
 
 #endif
