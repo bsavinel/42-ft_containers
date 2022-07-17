a1a8db6b55ba7aefb5e62c30f88c0b5350eeb579
Benoit Savinel <bsavinel@student.42.fr>
2022-07-17 20:15:30 +0200

    implementation lexi_comp

--- includes/utils/lexicographical_compare.hpp
+++ includes/utils/lexicographical_compare.hpp
@@ -6,7 +6,7 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 11:04:07 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/07/14 11:04:08 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/07/15 11:04:51 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,35 @@
 
 namespace ft
 {
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
 
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(first2, first1))
+				return false;
+			else if (comp(first1, first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
 }
 
 #endif
