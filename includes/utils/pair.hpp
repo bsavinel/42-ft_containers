9bf23f031e4c92c61c1edd0fd726629e8153f487
Benoit Savinel <bsavinel@student.42.fr>
2022-07-17 20:17:03 +0200

    implemete pair

--- includes/utils/pair.hpp
+++ includes/utils/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 11:05:52 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/07/14 11:05:57 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/07/17 19:02:20 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,78 @@
 
 namespace ft
 {
+	template <class T1, class T2>
+	struct pair
+	{
+		typedef T1 first_type;
+		typedef T2 second_type;
 
+		first_type first;
+		second_type second;
+
+		pair()
+		{
+		}
+		
+		pair(const first_type a, const second_type b): first(a), second(b)
+		{
+		}
+		
+		template<class U, class V>
+		pair (const pair<U,V>& pr): first(pr.first), second(pr.second)
+		{
+		}
+		
+		~pair()
+		{
+		}
+
+		pair& operator= (const pair& pr)
+		{
+			first = pr.first;
+			second = pr.second;
+		}
+	};
+
+	template <class T1, class T2>
+	inline bool operator== (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return lhs.first == rhs.first && lhs.second == rhs.second;
+	}
+
+	template <class T1, class T2>
+	inline bool operator!= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	template <class T1, class T2>
+	inline bool operator<  (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return lhs.first < rhs.first || (rhs.first == lhs.first && lhs.second<rhs.second);
+	}
+	//*		lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+	//*	=>	lhs.first < rhs.first || (!(lhs.first > rhs.first) && lhs.second < rhs.second);
+	//* =>	lhs.first < rhs.first || (lhs.first <= rhs.first && lhs.second < rhs.second);
+	//*	=>	lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second);
+
+	template <class T1, class T2>
+	inline bool operator<= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return !(rhs < lhs);
+	}
+
+	template <class T1, class T2>
+	inline bool operator>  (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return rhs < lhs;
+	}
+
+	template <class T1, class T2>
+	inline bool operator>= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return !(lhs < rhs);
+	}
 }
 
 #endif
