50bcb56242c78988b45d510a016ff5366c4690fa
Benoit Savinel <bsavinel@student.42.fr>
2022-09-21 22:34:00 +0200

    changement majeu de l'iterateur

--- includes/containers/iterator_map.hpp
+++ includes/containers/iterator_map.hpp
@@ -6,7 +6,7 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 10:43:42 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/09/21 15:48:45 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/09/21 22:31:07 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,153 @@
 namespace ft
 {
 	template <class Key, class T, class Compare, class Alloc>
-	class constIterator_map;
+		class constIterator_map;
+
+	template <class Key, class T, class Compare, class Alloc>
+		class Iterator_map;
+
+	template <class Key, class T, class Compare, class Alloc>
+	class IteratorBase_map
+	{
+		public:
+			typedef	ptrdiff_t						difference_type;
+			typedef	typename ft::pair<const Key, T>	value_type;
+			typedef	value_type*						pointer;
+			typedef	value_type&						reference;
+			typedef	random_access_iterator_tag		iterator_category;
+
+		private:
+			typedef	typename ft::IteratorBase_map<Key, T, Compare, Alloc>	iteratorBase_type;
+			typedef	typename ft::Iterator_map<Key, T, Compare, Alloc>		iterator_type;
+			typedef	typename ft::constIterator_map<Key, T, Compare, Alloc>	constIterator_type;
+			typedef	typename ft::RBT_node<value_type, Alloc>				node;
+			typedef	typename ft::RBT<value_type, Compare, Alloc>			RBTtree;
+
+		public:
+            IteratorBase_map()
+			{
+			}
+
+			IteratorBase_map(const RBTtree *tree, node *sentinel, node *start, int sensDepassement)
+			{
+				_tree = tree;
+				_sentinel = sentinel;
+				_current = start;
+				_sensDepassement = sensDepassement;
+			}
+
+			IteratorBase_map(const iteratorBase_type& rhs)
+			{
+				*this = rhs;
+			}
+				
+
+			~IteratorBase_map()
+			{
+			}
+
+			iteratorBase_type& operator=(const iteratorBase_type& rhs)
+			{
+				this->_current = rhs._current;
+				this->_sentinel = rhs._sentinel;
+				this->_tree = rhs._tree;
+				this->_sensDepassement = rhs._sensDepassement;
+				return *this;
+			}
+
+			iteratorBase_type& operator++()
+			{
+				if (_current == _sentinel)
+				{
+					if (_sensDepassement == -1)
+						_current = _tree->minimum();
+					return *this;
+				}
+				_sensDepassement = 1;
+				if (_current->_right != _sentinel)
+				{
+					_current = _current->_right;
+					while (_current->_left != _sentinel)
+						_current = _current->_left;
+					return *this;
+				}
+				while (_current->_parent != _sentinel && _current->_parent->_right == _current)
+					_current = _current->_parent;
+				_current = _current->_parent;
+				return *this;
+			}
+
+			iteratorBase_type  operator++(int)
+			{
+				iteratorBase_type tmp = *this;
+				++(*this);
+				return tmp;
+			}
+		
+			iteratorBase_type& operator--()
+			{
+				if (_current == _sentinel)
+				{
+					if (_sensDepassement == 1)
+						_current = _tree->maximum();
+					return *this;
+				}
+				_sensDepassement = -1;
+				if (_current->_left != _sentinel)
+				{
+					_current = _current->_left;
+					while (_current->_right != _sentinel)
+						_current = _current->_right;
+					return *this;
+				}
+				while (_current->_parent != _sentinel && _current->_parent->_left == _current)
+					_current = _current->_parent;
+				_current = _current->_parent;
+				return *this;
+			}
+
+			iteratorBase_type  operator--(int)
+			{
+				iteratorBase_type tmp = *this;
+				--(*this);
+				return tmp;
+			}
+			
+			reference operator*() const
+			{
+				return _current->_value;
+			}
+		
+			pointer operator->() const
+			{
+				return &(operator*());
+			}
+
+			template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+			friend inline bool operator== (const IteratorBase_map<Key1, T1, Compare1, Alloc1>& lhs, const IteratorBase_map<Key2, T2, Compare2, Alloc2>& rhs)
+			{
+				if (lhs._current == rhs._current && lhs._sentinel == rhs._sentinel && lhs._tree == rhs._tree)
+				{
+					if (lhs._current == lhs._sentinel && lhs._sensDepassement != rhs._sensDepassement)
+						return false;
+					return true;
+				}
+				return false;
+			}
+
+			template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+			friend inline bool operator!= (const IteratorBase_map<Key1, T1, Compare1, Alloc1>& lhs, const IteratorBase_map<Key2, T2, Compare2, Alloc2>& rhs)
+			{
+				return !(lhs == rhs);
+			}
+		
+		private:
+			RBTtree	*_tree;
+			node	*_sentinel;
+			node	*_current;
+			int		_sensDepassement;
+	};
+
 	//! --------------------------------------- iterator map ----------------------------------
 	
 	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<T> >
@@ -35,6 +181,7 @@ namespace ft
 			typedef	random_access_iterator_tag		iterator_category;
 
 		private:
+			typedef	typename ft::IteratorBase_map<Key, T, Compare, Alloc>	iteratorBase_type;
 			typedef	typename ft::Iterator_map<Key, T, Compare, Alloc>		iterator_type;
 			typedef	typename ft::constIterator_map<Key, T, Compare, Alloc>	constIterator_type;
 			typedef	typename ft::RBT_node<value_type, Alloc>				node;
@@ -45,8 +192,9 @@ namespace ft
 			{
 			}
 				
-			Iterator_map(RBTtree *tree, node *sentinel, node *start, int sensDepassement): _tree(tree), _sentinel(sentinel), _current(start), _sensDepassement(sensDepassement)
+			Iterator_map(const RBTtree *tree, node *sentinel, node *start, int sensDepassement)
 			{
+				_current = iteratorBase_type(tree, sentinel, start, sensDepassement);
 			}
 
 			Iterator_map(const iterator_type& rhs)
@@ -54,225 +202,43 @@ namespace ft
 				*this = rhs;
 			}
 
+			Iterator_map(const constIterator_type& rhs)
+			{
+				_current = rhs.base();
+			}
+
 			~Iterator_map()
 			{
 			}
 			
+			const iteratorBase_type &base() const
+			{
+				return _current;
+			}
+			
 			iterator_type& operator=(const iterator_type& rhs)
 			{
-				this->_current = rhs._current;
-				this->_sentinel = rhs._sentinel;
-				this->_tree = rhs._tree;
-				this->_sensDepassement = rhs._sensDepassement;
-				return *this;
-			}
-
-			reference operator*() const
-			{
-				return _current->_value;
+				_current = rhs.base();
+                return *this;
 			}
 
 			iterator_type& operator++()
 			{
-				if (_current == _sentinel)
-				{
-					if (_sensDepassement == -1)
-						_current = _tree->minimum();
-					return *this;
-				}
-				_sensDepassement = 1;
-				if (_current->_right != _sentinel)
-				{
-					_current = _current->_right;
-					while (_current->_left != _sentinel)
-						_current = _current->_left;
-					return *this;
-				}
-				while (_current->_parent != _sentinel && _current->_parent->_right == _current)
-					_current = _current->_parent;
-				_current = _current->_parent;
+				++_current;
 				return *this;
 			}
 
 			iterator_type  operator++(int)
 			{
 				iterator_type tmp = *this;
+
 				++(*this);
 				return tmp;
 			}
 		
 			iterator_type& operator--()
 			{
-				if (_current == _sentinel)
-				{
-					if (_sensDepassement == 1)
-						_current = _tree->maximum();
-					return *this;
-				}
-				_sensDepassement = -1;
-				if (_current->_left != _sentinel)
-				{
-					_current = _current->_left;
-					while (_current->_right != _sentinel)
-						_current = _current->_right;
-					return *this;
-				}
-				while (_current->_parent != _sentinel && _current->_parent->_left == _current)
-					_current = _current->_parent;
-				_current = _current->_parent;
-				return *this;
-			}
-
-			iterator_type  operator--(int)
-			{
-				iterator_type tmp = *this;
-				--(*this);
-				return tmp;
-			}
-		
-			pointer operator->() const
-			{
-				return &(operator*());
-			}
-			
-			friend bool operator== (const iterator_type& lhs, const iterator_type& rhs)
-			{
-				if (lhs._current == rhs._current && lhs._sentinel == rhs._sentinel && lhs._tree == rhs._tree)
-				{
-					if (lhs._current == lhs._sentinel && lhs._sensDepassement != rhs._sensDepassement)
-						return false;
-					return true;
-				}
-				return false;
-			}
-			
-			friend bool operator== (const iterator_type& lhs, const constIterator_type& rhs)
-			{
-				if (lhs._current == rhs._current && lhs._sentinel == rhs._sentinel && lhs._tree == rhs._tree)
-				{
-					if (lhs._current == lhs._sentinel && lhs._sensDepassement != rhs._sensDepassement)
-						return false;
-					return true;
-				}
-				return false;
-			}
-
-			friend bool operator!= (const iterator_type& lhs, const iterator_type& rhs)
-			{
-				return !(lhs == rhs);
-			}
-
-			friend bool operator!= (const iterator_type& lhs, const constIterator_type& rhs)
-			{
-				return !(lhs == rhs);
-			}
-
-		private:
-			RBTtree	*_tree;
-			node	*_sentinel;
-			node	*_current;
-			int		_sensDepassement;
-			
-	};
-
-	//! --------------------------------- Const iterator map -------------------------------
-
-	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<T> >
-	class constIterator_map
-	{
-		public:
-			typedef	ptrdiff_t								difference_type;
-			typedef	const typename ft::pair<const Key, T>	value_type;
-			typedef	const value_type*						pointer;
-			typedef	const value_type&						reference;
-			typedef	random_access_iterator_tag				iterator_category;
-		
-		private:
-			typedef	typename ft::Iterator_map<Key, T, Compare, Alloc>				iterator_type;
-			typedef	typename ft::constIterator_map<Key, T, Compare, Alloc>			constIterator_type;
-			typedef	const typename ft::RBT_node<ft::pair<const Key, T>, Alloc>		node;
-			typedef	const typename ft::RBT<ft::pair<const Key, T>, Compare, Alloc>	RBTtree;
-		
-		public:
-			constIterator_map()
-			{
-			}
-				
-			constIterator_map(RBTtree *tree, node *sentinel, node *start, int sensDepassement): _tree(tree), _sentinel(sentinel), _current(start), _sensDepassement(sensDepassement)
-			{
-			}
-
-            constIterator_map(const constIterator_type& rhs)
-            {
-				*this = rhs;
-			}
-
-			~constIterator_map()
-			{
-			}
-			
-			constIterator_type& operator=(const constIterator_type& rhs)
-			{
-				this->_current = rhs._current;
-				this->_sentinel = rhs._sentinel;
-				this->_tree = rhs._tree;
-				this->_sensDepassement = rhs._sensDepassement;
-				return *this;
-			}
-
-			reference operator*() const
-			{
-				return _current->_value;
-			}
-
-			constIterator_type& operator++()
-			{
-				if (_current == _sentinel)
-				{
-					if (_sensDepassement == -1)
-						_current = _tree->minimum();
-					return *this;
-				}
-				_sensDepassement = 1;
-				if (_current->_right != _sentinel)
-				{
-					_current = _current->_right;
-					while (_current->_left != _sentinel)
-						_current = _current->_left;
-					return *this;
-				}
-				while (_current->_parent != _sentinel && _current->_parent->_right == _current)
-					_current = _current->_parent;
-				_current = _current->_parent;
-				return *this;
-			}
-
-			constIterator_type  operator++(int)
-			{
-				constIterator_type tmp = *this;
-				++(*this);
-				return tmp;
-			}
-		
-			constIterator_type& operator--()
-			{
-				if (_current == _sentinel)
-				{
-					if (_sensDepassement == 1)
-						_current = _tree->maximum();
-					return *this;
-				}
-				_sensDepassement = -1;
-				if (_current->_left != _sentinel)
-				{
-					_current = _current->_left;
-					while (_current->_right != _sentinel)
-						_current = _current->_right;
-					return *this; 
-				}
-				while (_current->_parent != _sentinel && _current->_parent->_left == _current)
-					_current = _current->_parent;
-				_current = _current->_parent;
+				--_current;
 				return *this;
 			}
 
@@ -282,51 +248,163 @@ namespace ft
 				--(*this);
 				return tmp;
 			}
+
+			reference operator*() const
+			{
+				return  *_current;
+			}
+
+			pointer operator->() const
+			{
+				return &(operator*());
+			}
+
+		private:
+			iteratorBase_type _current;
+	};
+
+	//! --------------------------------- Const iterator map -------------------------------
+
+	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<T> >
+	class constIterator_map
+	{
+		public:
+			typedef	ptrdiff_t								difference_type;
+			typedef	typename ft::pair<const Key, T>			value_type;
+			typedef	const value_type*						pointer;
+			typedef	const value_type&						reference;
+			typedef	random_access_iterator_tag				iterator_category;
+		
+		private:
+			typedef	typename ft::IteratorBase_map<Key, T, Compare, Alloc>	iteratorBase_type;
+			typedef	typename ft::Iterator_map<Key, T, Compare, Alloc>		iterator_type;
+			typedef	typename ft::constIterator_map<Key, T, Compare, Alloc>	constIterator_type;
+			typedef	typename ft::RBT_node<value_type, Alloc>				node;
+			typedef	typename ft::RBT<value_type, Compare, Alloc>			RBTtree;
+		
+		public:
+			constIterator_map()
+			{
+			}
+				
+			constIterator_map(const RBTtree *tree, node *sentinel, node *start, int sensDepassement)
+			{
+				_current = iteratorBase_type(tree, sentinel, start, sensDepassement);
+			}
+
+            constIterator_map(const constIterator_type& rhs)
+            {
+				*this = rhs;
+			}
+
+			constIterator_map(const iterator_type& rhs)
+			{
+				_current = rhs.base();
+			}
+
+			~constIterator_map()
+			{
+			}
+
+			const iteratorBase_type &base() const
+			{
+				return _current;
+			}
+			
+			constIterator_type& operator=(const constIterator_type& rhs)
+			{
+				_current = rhs.base();
+				return *this;
+			}
+
+			constIterator_type& operator++()
+			{
+				++_current;
+				return *this;
+			}
+
+			constIterator_type  operator++(int)
+			{
+				constIterator_type tmp = *this;
+
+				++(*this);
+				return tmp;
+			}
+		
+			constIterator_type& operator--()
+			{
+				return *_current;
+			}
+
+			constIterator_type  operator--(int)
+			{
+				constIterator_type tmp = *this;
+				--(*this);
+				return tmp;
+			}
+
+			reference operator*() const
+			{
+				return  *_current;
+			}
 		
 			pointer operator->() const
 			{
 				return &(operator*());
 			}
-			
-			friend bool operator== (const constIterator_type& lhs, const constIterator_type& rhs)
-			{
-				//std::cout << "entre  dans operateur" << std::endl;
-				if (lhs._current == rhs._current && lhs._sentinel == rhs._sentinel && lhs._tree == rhs._tree)
-				{
-					if (lhs._current == lhs._sentinel && lhs._sensDepassement != rhs._sensDepassement)
-						return false;
-					return true;
-				}
-				return false;
-			}
-
-			friend bool operator== (const constIterator_type& lhs, const iterator_type& rhs)
-			{
-				if (lhs._current == rhs._current && lhs._sentinel == rhs._sentinel && lhs._tree == rhs._tree)
-				{
-					if (lhs._current == lhs._sentinel && lhs._sensDepassement != rhs._sensDepassement)
-						return false;
-					return true;
-				}
-				return false;
-			}
-			
-			friend bool operator!= (const constIterator_type& lhs, const constIterator_type& rhs)
-			{
-				return !(lhs == rhs);
-			}
-			
-			friend bool operator!= (const constIterator_type& lhs, const iterator_type& rhs)
-			{
-				return !(lhs == rhs);
-			}
 
 		private:
-			RBTtree	*_tree;
-			node	*_sentinel;
-			node	*_current;
-			int		_sensDepassement;
+			iteratorBase_type _current;
 	};
+
+			
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator== (const Iterator_map<Key1, T1, Compare1, Alloc1>& lhs, const Iterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() == rhs.base();
+	}
+
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator== (const Iterator_map<Key1, T1, Compare1, Alloc1>& lhs, const constIterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() == rhs.base();
+	}
+	
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator== (const constIterator_map<Key1, T1, Compare1, Alloc1>& lhs, const constIterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() == rhs.base();
+	}
+
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator== (const constIterator_map<Key1, T1, Compare1, Alloc1>& lhs, const Iterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() == rhs.base();
+	}
+
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator!= (const Iterator_map<Key1, T1, Compare1, Alloc1>& lhs, const Iterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() != rhs.base();
+	}
+
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator!= (const Iterator_map<Key1, T1, Compare1, Alloc1>& lhs, const constIterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() != rhs.base();
+	}
+
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator!= (const constIterator_map<Key1, T1, Compare1, Alloc1>& lhs, const constIterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() != rhs.base();
+	}
+			
+	template<class Key1, class T1, class Compare1, class Alloc1, class Key2, class T2, class Compare2, class Alloc2>
+	inline bool operator!= (const constIterator_map<Key1, T1, Compare1, Alloc1>& lhs, const Iterator_map<Key2, T2, Compare2, Alloc2>& rhs)
+	{
+		return lhs.base() != rhs.base();
+	}
 }
 
 #endif
--- testeur/srcs/containers/map_test.cpp
+++ testeur/srcs/containers/map_test.cpp
@@ -6,7 +6,7 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 12:11:59 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/09/19 09:47:15 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/09/21 20:17:49 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,64 +117,38 @@ void	printReverse(TESTED_NAMESPACE::map<T1, T2> &mp)
 	std::cout << "_______________________________________________" << std::endl;
 }
 
-#define T1 int
-#define T2 foo<int>
-typedef TESTED_NAMESPACE::map<T1, T2>::value_type T3;
-typedef TESTED_NAMESPACE::map<T1, T2>::iterator ft_iterator;
-typedef TESTED_NAMESPACE::map<T1, T2>::const_iterator ft_const_iterator;
 
-static int iter = 0;
+#define T1 char
+#define T2 foo<float>
+typedef TESTED_NAMESPACE::map<T1, T2> _map;
+typedef _map::const_iterator const_it;
 
-template <typename MAP>
-void	ft_bound(MAP &mp, const T1 &param)
+static unsigned int i = 0;
+
+void	ft_comp(const _map &mp, const const_it &it1, const const_it &it2)
 {
-	ft_iterator ite = mp.end(), it[2];
-	_pair<ft_iterator, ft_iterator> ft_range;
+	bool res[2];
 
-	std::cout << "\t-- [" << iter++ << "] --" << std::endl;
-	std::cout << "with key [" << param << "]:" << std::endl;
-	it[0] = mp.lower_bound(param); it[1] = mp.upper_bound(param);
-	ft_range = mp.equal_range(param);
-	std::cout << "lower_bound: " << (it[0] == ite ? "end()" : printPair(it[0], false)) << std::endl;
-	std::cout << "upper_bound: " << (it[1] == ite ? "end()" : printPair(it[1], false)) << std::endl;
-	std::cout << "equal_range: " << (ft_range.first == it[0] && ft_range.second == it[1]) << std::endl;
+	std::cout << "\t-- [" << ++i << "] --" << std::endl;
+	res[0] = mp.key_comp()(it1->first, it2->first);
+	res[1] = mp.value_comp()(*it1, *it2);
+	std::cout << "with [" << it1->first << " and " << it2->first << "]: ";
+	std::cout << "key_comp: " << res[0] << " | " << "value_comp: " << res[1] << std::endl;
 }
 
-template <typename MAP>
-void	ft_const_bound(const MAP &mp, const T1 &param)
+void		map_test()
 {
-	ft_const_iterator ite = mp.end(), it[2];
-	_pair<ft_const_iterator, ft_const_iterator> ft_range;
+	_map	mp;
 
-	std::cout << "\t-- [" << iter++ << "] (const) --" << std::endl;
-	std::cout << "with key [" << param << "]:" << std::endl;
-	it[0] = mp.lower_bound(param); it[1] = mp.upper_bound(param);
-	ft_range = mp.equal_range(param);
-	std::cout << "lower_bound: " << (it[0] == ite ? "end()" : printPair(it[0], false)) << std::endl;
-	std::cout << "upper_bound: " << (it[1] == ite ? "end()" : printPair(it[1], false)) << std::endl;
-	std::cout << "equal_range: " << (ft_range.first == it[0] && ft_range.second == it[1]) << std::endl;
-}
-
-void	map_test()
-{
-	std::list<T3> lst;
-	unsigned int lst_size = 10;
-	for (unsigned int i = 0; i < lst_size; ++i)
-		lst.push_back(T3(i + 1, (i + 1) * 3));
-	TESTED_NAMESPACE::map<T1, T2> mp(lst.begin(), lst.end());
+	mp['a'] = 2.3;
+	mp['b'] = 1.4;
+	mp['c'] = 0.3;
+	mp['d'] = 4.2;
 	printSize(mp);
 
-	ft_const_bound(mp, -10);
-	ft_const_bound(mp, 1);
-	ft_const_bound(mp, 5);
-	ft_const_bound(mp, 10);
-	ft_const_bound(mp, 50);
-
-	printSize(mp);
-	mp.lower_bound(3)->second = 404;
-	mp.upper_bound(7)->second = 842;
-	ft_bound(mp, 5);
-	ft_bound(mp, 7);
+	for (const_it it1 = mp.begin(); it1 != mp.end(); ++it1)
+		for (const_it it2 = mp.begin(); it2 != mp.end(); ++it2)
+			ft_comp(mp, it1, it2);
 
 	printSize(mp);
 }
